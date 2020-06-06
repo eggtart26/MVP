f5c0cb72edfaec74b5a7f1f23d26cb230ddfd509
eggtart26 <justin.guan26@gmail.com>
2020-06-06 12:41:21 -0700

    environment set up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>From APP.JSX</div>
+        )
+    }
+} 
+
+
+export default App;
